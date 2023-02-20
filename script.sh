a5570f10a57cf4c96061c1da1d5c6a303e9b348b
Bernat <116073301+btort24@users.noreply.github.com>
2023-02-20 13:03:21 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -26,7 +26,7 @@ RAM=$(vmstat -s -S M | grep "total memory" | cut -c 1-16)
 HDD=$(df -h -t ext4 | sort -k 2 | head -1)
 echo "+---------------------------------------------+"
 echo -e "NOM ALUMNE:${GREEN} ${nom}${NONE}"
-echo -e "NOM ALUMNE:${nom}" > /var/log/examen.txt
+echo -e "NOM ALUMNE:${nom}" >> /var/log/examen.txt
 echo -e "Dades xarxa (HOST / Gateway / DNS):${GREEN} ${IP} / ${GATE} / ${DNS}${NONE}"
 echo -e "Dades xarxa (HOST / Gateway / DNS): ${IP} / ${GATE} / ${DNS}" >> /var/log/examen.txt
 echo -e "Característiques (RAM / HDD):${GREEN} ${RAM} / ${HDD} ${NONE}"
@@ -55,60 +55,71 @@ echo -e "[*]  Final de la insta.lació: $DATA_FI " >> /var/log/examen.txt
 echo
 echo "+---------------------------------------------+"
 echo "+---------------------------------------------+" >> /var/log/examen.txt
+
+#Comprovem host
+if [ "$IP" != "172.31.0.250/24" ]; then
+	echo -e "[*]  ${RED}Ip Host Incorrecte!! -- ${IP} enlloc de 172.31.0.250/24${NONE}"
+	echo -e "[*]  Ip host Incorrecte!! ${IP} ---  172.31.0.250/24" >> /var/log/examen.txt
+else
+	punts=$((punts + 4));
+	echo -e "[*]  ${GREEN}La ip host és correcte${IP} ---  172.31.0.250/24 ${NONE}"
+	echo -e "[*]  La ip host és correcte${IP} ---  172.31.0.250/24" >> /var/log/examen.txt
+fi
+
 #Comprovem apache2
 if [ $(dpkg-query -W -f='${Status}' apache2 2>/dev/null | grep -c "ok installed") -eq 0 ] >>/var/log/examen.txt;
 	then
 	echo -e "[*]  Apache2 no està instal.lat                             ${RED}Incorrecte!!${NONE}"
-	echo -e "[*]  Apache2 no està instal.lat:  Correcte!! 0 punts${NONE}" >> /var/log/examen.txt
+	echo -e "[*]  Apache2 no està instal.lat:                            Incorrecte!!${NONE}" >> /var/log/examen.txt
 	else
 #	punts=$((punts + 1));
 	echo -e "[*]  Apache2 està instal.lat                                ${GREEN}Correcte!!${NONE}" 
-	echo -e "[*]  Apache2 està instal.lat:   Correcte!" >> /var/log/examen.txt
+	echo -e "[*]  Apache2 està instal.lat:                               Correcte!!" >> /var/log/examen.txt
 fi
 
 #Comprovem Mariadb-Server
 if [ $(dpkg-query -W -f='${Status}' mariadb-server 2>/dev/null | grep -c "ok installed") -eq 0 ];
 	then
 	echo -e "[*]  Mariadb-Server no està instal.lat                      ${RED}Incorrecte!!${NONE}"
-	echo -e "[*]  Mariadb-Server no està instal.lat   Incorrecte!! 0 punts" >> /var/log/examen.txt
+	echo -e "[*]  Mariadb-Server no està instal.lat                      Incorrecte!!" >> /var/log/examen.txt
 	else
 #	punts=$((punts + 1));
 	echo -e "[*]  Mariadb-Server està instal.lat                         ${GREEN}Correcte!!${NONE}"
-	echo -e "[*]  Mariadb-Server està instal.lat:    Correcte!" >> /var/log/examen.txt
+	echo -e "[*]  Mariadb-Server està instal.lat:                        Correcte!!" >> /var/log/examen.txt
 fi
 
 #Comprovem php
 if [ $(dpkg-query -W -f='${Status}' php 2>/dev/null | grep -c "ok installed") -eq 0 ];
 	then
 	echo -e "[*]  PHP no està instal.lat                                 ${RED}Incorrecte!! 0 punts${NONE}"
-	echo -e "[*]  PHP no està instal.lat: Incorrecte!! 0 punts" >> /var/log/examen.txt
+	echo -e "[*]  PHP no està instal.lat:                                Incorrecte!!" >> /var/log/examen.txt
 	else
 #	punts=$((punts + 3));
-	echo -e "[*]  PHP està instal.lat                                    ${GREEN}Correcte!${NONE}"
-	echo -e "[*]  PHP està instal.lat: Correcte!" >> /var/log/examen.txt
+	echo -e "[*]  PHP està instal.lat                                    ${GREEN}Correcte!!${NONE}"
+	echo -e "[*]  PHP està instal.lat:                                   Correcte!!" >> /var/log/examen.txt
 fi
+
 
 #Comprovem php-mysql
 if [ $(dpkg-query -W -f='${Status}' php-mysql 2>/dev/null | grep -c "ok installed") -eq 0 ];
 	then
 	echo -e "[*]  El paquet PHP-MYSQL no està instal.lat                 ${RED}Incorrecte!! 0 punts${NONE}"
-	echo -e "[*]  El paquet PHP-MYSQL no està instal.lat:   Incorrecte!! 0 punts" >> /var/log/examen.txt
+	echo -e "[*]  El paquet PHP-MYSQL no està instal.lat:                Incorrecte!!" >> /var/log/examen.txt
 	else
-	punts=$((punts + 5));
-	echo -e "[*]  Si la xarxa NAT és correcte i,                         ${GREEN} ${IP} és igual a: 172.31.0.250 ${NONE}
-	echo -e "[*]  El paquet PHP-MYSQL està instal.lat                    ${GREEN}Correcte! $punts${NONE}"
-	echo -e "[*]  El paquet PHP-MYSQL està instal.lat: Correcte! $punts" >> /var/log/examen.txt
+	punts=$((punts + 1));
+	echo -e "[*]  El paquet PHP-MYSQL està instal.lat                    ${GREEN}Correcte!! $punts${NONE}"
+	echo -e "[*]  El paquet PHP-MYSQL està instal.lat:                   Correcte!! $punts" >> /var/log/examen.txt
 fi
 
 #Comprovem base de dades
-DBNAME="glpi"
+DBNAME="moodle"
 if [ -d "/var/lib/mysql/$DBNAME" ]; then
 	punts=$((punts + 1));
-	echo -e "[*]  La base de dades $DBNAME existeix.                   ${GREEN}Correcte! $punts"
-	echo -e "[*]  La base de dades $DBNAME existeix:  Correcte! $punts" >> /var/log/examen.txt
+	echo -e "[*]  La base de dades $DBNAME existeix:                      ${GREEN}Correcte!! $punts"
+	echo -e "[*]  La base de dades $DBNAME existeix:                      Correcte!! $punts" >> /var/log/examen.txt
 else
-	echo -e "[*]  La base de dades $DBNAME no existeix.                ${RED}Incorrecte!! 0 punts${NONE}"
-	echo -e "[*]  La base de dades $DBNAME no existeix:  Incorrecte!! 0 punts" >> /var/log/examen.txt
+	echo -e "[*]  La base de dades $DBNAME no existeix.                ${RED}Incorrecte!! ${NONE}"
+	echo -e "[*]  La base de dades $DBNAME no existeix:                Incorrecte!!" >> /var/log/examen.txt
 fi
 
 #Comprovem descompressió fitxer
@@ -116,7 +127,7 @@ WGET="/opt/"
 
 if [ ! "$(ls $WGET)" ]; then
 	echo -e "[*]  No has fet la descarrega.                              ${RED}Incorrecte!!${NONE}"
-	echo -e "[*]  No has fet la descarrega:  Incorrecte!!" >> /var/log/examen.txt
+	echo -e "[*]  No has fet la descarrega:                              Incorrecte!!" >> /var/log/examen.txt
 echo "+-----------------------------------------------------------------------------+"
 echo "+-----------------------------------------------------------------------------+"
 echo -e "${GREEN}La nota de l'examen és: $punts${NONE}"
@@ -126,8 +137,8 @@ echo "+-------------------------------------------------------------------------
 	exit
 	else
 	punts=$((punts + 1));
-	echo -e "[*]  Has fet la descarrega.                                 ${GREEN}Correcte! $punts${NONE}"
-	echo -e "[*]  Has fet la descarrega:  Correcte! $punts" >> /var/log/examen.txt
+	echo -e "[*]  Has fet la descarrega.                                 ${GREEN}Correcte!! $punts${NONE}"
+	echo -e "[*]  Has fet la descarrega:                                 Correcte!! $punts" >> /var/log/examen.txt
 fi
 
 #Comprovem moure software decarregat al host
@@ -135,64 +146,76 @@ HOST_DIR="/var/www/html/"
 HOST_FILE="/var/www/html/index.html"
 
 if [ ! -d "$HOST_DIR" ];
-	then echo -e "[*]  El directori host no existeix.                    ${RED}Incorrecte!! 0 punts${NONE}"
-	echo -e "[*]  El directori host no existeix:  Incorrecte!! 0 punts" >> /var/log/examen.txt
+	then echo -e "[*]  El directori host no existeix.                    ${RED}Incorrecte!!${NONE}"
+	echo -e "[*]  El directori host no existeix:                         Incorrecte!!" >> /var/log/examen.txt
 	else
 	if [  "$(ls $HOST_DIR)" ];
 	then echo -e "El directori té algun fitxer -- index.html i és?"
 	echo -e "El directori té algun fitxer -- index.html i és?" >> /var/log/examen.txt
 		if [ ! -f "$HOST_FILE" ]
 #		punts=$((punts + 1));
-		then echo -e "[*]  Fitxer index.html borrat.                              ${GREEN}Correcte! ${NONE}"
-		echo -e "[*]  Fitxer index.html borrat:  Correcte! $punts" >> /var/log/examen.txt
-		else echo -e "[*]  No has esborrat el fitxer index.html!!                 ${RED}Incorrecte!! 0 punts${NONE}"
-		echo -e "[*]  No has esborrat el fitxer index.html!! Incorrecte!! 0 punts" >> /var/log/examen.txt
+		then echo -e "[*]  Fitxer index.html borrat.                              ${GREEN}Correcte!! ${NONE}"
+		echo -e "[*]  Fitxer index.html borrat:                                   Correcte!!" >> /var/log/examen.txt
+		else 
+		punts=$((punts -1));
+		echo -e "[*]  No has esborrat el fitxer index.html!!                 ${RED}Incorrecte!! $punts${NONE}"
+		echo -e "[*]  No has esborrat el fitxer index.html!!                 Incorrecte!! $punts" >> /var/log/examen.txt
 		fi
 	else
-	echo -e "[*]  El directori host està buit.                               ${RED}Incorrecte!! 0 punts${NONE}" 
-	echo -e "[*]  El directori host està buit: Incorrecte!! 0 punts" >> /var/log/examen.txt
+	echo -e "[*]  El directori host està buit.                               ${RED}Incorrecte!!${NONE}" 
+	echo -e "[*]  El directori host està buit:                               Incorrecte!!" >> /var/log/examen.txt
 fi
 fi
 
 #Comprovem els permisos
-#HOST_DIR="/var/www/html/"
-
-if [ -d "$HOST_DIR" ] && [ $(stat -c "%a" $HOST_DIR) == "755" ]; then
-	punts=$((punts + 1));
-	echo -e "[*]  El directori host té permisos correctes.               ${GREEN}Correcte! $punts${NONE}"
-	echo -e "[*]  El directori host té permisos correctes:   Correcte! $punts" >> /var/log/examen.txt
-	else
-	echo -e "[*]  El directori host no té permisos 755.                  ${RED}Incorrecte!! 0 punts${NONE}"
-	echo -e "[*]  El directori host no té permisos 755:  Incorrecte!! 0 punts" >> /var/log/examen.txt
+HOST_DIR="/var/www/html/"
+if [  "$(ls $HOST_DIR)" ];
+	then echo -e "Comprovació dels permisos"
+	if [ -d "$HOST_DIR" ] && [ $(stat -c "%a" $HOST_DIR) == "755" ]; then
+#		punts=$((punts + 1));
+		echo -e "[*]  El directori host té permisos correctes.               ${GREEN}Correcte!! $punts${NONE}"
+		echo -e "[*]  El directori host té permisos correctes:               Correcte!!" >> /var/log/examen.txt
+		else
+		echo -e "[*]  El directori host no té permisos 755.                  ${RED}Incorrecte!!${NONE}"
+		echo -e "[*]  El directori host no té permisos 755:                  Incorrecte!!" >> /var/log/examen.txt
+	fi
 fi
-
 
 #Comprovem si s'ha creat el directori moodledata
 HOST_DIR_MDATA="/var/www/moodledata"
 
 if [ ! -d "$HOST_DIR_MDATA" ]
-	then echo -e "[*]  El directori moodledata no existeix.                   ${RED}Incorrecte!! 0 punts${NONE}"
-	echo -e "[*]  El directori moodledata no existeix:  Incorrecte!! 0 punts" >> /var/log/examen.txt
-	else echo -e "[*]  El directori moodledata existeix.                      ${GREEN}Correcte! ${NONE}"
-	echo -e "[*]  El directori moodledata existeix:     Correcte!" >> /var/log/examen.txt
+	then
+	punts=$((punts -1));
+	echo -e "[*]  El directori moodledata no existeix.                   ${RED}Incorrecte!! $punts${NONE}"
+	echo -e "[*]  El directori moodledata no existeix:                   Incorrecte!! $punts" >> /var/log/examen.txt
+	else echo -e "[*]  El directori moodledata existeix.                      ${GREEN}Correcte!! ${NONE}"
+	echo -e "[*]  El directori moodledata existeix:                           Correcte!!" >> /var/log/examen.txt
 fi
 
 #Comprovem propietaris
-if [ -d "$HOST_DIR" ] && [ $(stat -c "%U" $HOST_DIR) == "www-data" ]; then
-#	punts=$((punts + 1));
-	echo -e "[*]  El directori host té usuari correcte.                  ${GREEN}Correcte! ${NONE}"
-	echo -e "[*]  El directori host té usuari correcte:  Correcte! $punts"  >> /var/log/examen.txt
-else
-	echo -e "[*]  El directori host té usuari incorrecte.                ${RED}Incorrecte!! 0 punts${NONE}"
-	echo -e "[*]  El directori host té usuari incorrecte:  Incorrecte!! 0 punts" >> /var/log/examen.txt
+if [  "$(ls $HOST_DIR)" ];
+	then echo -e "Comprovació d'usuari fitxer"
+	if [ -d "$HOST_DIR" ] && [ $(stat -c "%U" $HOST_DIR) == "www-data" ]; then
+#		punts=$((punts + 1));
+		echo -e "[*]  El directori host té usuari correcte.                  ${GREEN}Correcte!! ${NONE}"
+		echo -e "[*]  El directori host té usuari correcte:                  Correcte!!"  >> /var/log/examen.txt
+	else
+		echo -e "[*]  El directori host té usuari incorrecte.                ${RED}Incorrecte!!${NONE}"
+		echo -e "[*]  El directori host té usuari incorrecte:                Incorrecte!!" >> /var/log/examen.txt
+	fi
 fi
-if [ -d "$HOST_DIR" ] && [ $(stat -c "%G" $HOST_DIR) == "www-data" ]; then
-	punts=$((punts + 1));
-	echo -e "[*]  El directori host té grup correcte                     ${GREEN}Correcte!! $punts${NONE}"
-	echo -e "[*]  El directori host té grup correcte:  Correcte!! $punts" >> /var/log/examen.txt
-else
-	echo -e "[*]  El directori host no té grup correcte                  ${RED}Incorrecte!! 0 punts${NONE}"
-	echo -e "[*]  El directori host no té grup correcte:  Incorrecte!! 0 punts" >> /var/log/examen.txt
+
+if [  "$(ls $HOST_DIR)" ];
+	then echo -e "Comprovació grup fitxer"
+	if [ -d "$HOST_DIR" ] && [ $(stat -c "%G" $HOST_DIR) == "www-data" ]; then
+		punts=$((punts + 1));
+		echo -e "[*]  El directori host té grup correcte                     ${GREEN}Correcte!! $punts${NONE}"
+		echo -e "[*]  El directori host té grup correcte:                    Correcte!! $punts" >> /var/log/examen.txt
+		else
+		echo -e "[*]  El directori host no té grup correcte                  ${RED}Incorrecte!!${NONE}"
+		echo -e "[*]  El directori host no té grup correcte:                 Incorrecte!!" >> /var/log/examen.txt
+	fi
 fi
 echo "+---------------------------------------------------------------------+"
 echo "+---------------------------------------------------------------------+"
@@ -200,6 +223,7 @@ echo -e "${GREEN}La nota de l'examen és: $punts${NONE}"
 echo -e "La nota de l'examen és: $punts" >> /var/log/examen.txt
 echo "+---------------------------------------------------------------------+"
 echo "+---------------------------------------------------------------------+"
+echo "+-------------------------------------------------------------------------------------------------------------------------+" >> /var/log/examen.txt
 
 apt-get -y update >/dev/null
 apt-get install -y sendemail >/dev/null
